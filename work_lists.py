771da199177e2d01256ccc27ee7e3a35c8d30e63
Barboss-sys <Iv4ov00095@yandex.com>
2025-01-21 09:08:02 +0300

    update

---
+++ work_lists.py
@@ -0,0 +1,16 @@
+
+
+
+pizzas = ['peppironi', 'balonese', 'meat']
+
+
+for pizza in pizzas:
+    print(f'I like {pizza.title()} pizza')
+
+print('I really love pizza!')
+
+animals = ['dog', 'caat', 'lama', 'cow']
+for animal in animals:
+    print(f'A {animal} would make a great pet')
+
+print('Any of these animals would make a great pet!')
